3756d85d0f02335f3af65e3c061e38716ce7d90e
Kudin Roman <kudinroman82@gmail.com>
2019-02-10 23:22:37 +0300

    add HomeController.cs, Index.cshtml

---
+++ ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View(new Dictionary<string, string>
+        {
+            ["Message"] = "This is the Index action"
+        });
+    }
+}

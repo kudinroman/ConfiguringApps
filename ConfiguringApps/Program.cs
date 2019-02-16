60c22e626a8a9a8d726966494a69f806330a8e05
Kudin Roman <kudinroman82@gmail.com>
2019-02-16 23:44:31 +0300

    Freeman A. Chapter 14 сompletion: Configuring Applications

--- ConfiguringApps/Program.cs
+++ ConfiguringApps/Program.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace ConfiguringApps
 {
@@ -14,11 +9,42 @@ namespace ConfiguringApps
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json",
+                        optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
+                        optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(
+                    hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .UseIISIntegration()
+                .UseDefaultServiceProvider((context, options) =>
+                {
+                    options.ValidateScopes =
+                    context.HostingEnvironment.IsDevelopment();
+                })
+                .UseStartup(nameof(ConfiguringApps))
+                .Build();
+        }
     }
 }
--- ConfiguringApps/Startup.cs
+++ ConfiguringApps/Startup.cs
@@ -1,34 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ConfiguringApps.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
-namespace ConfiguringApps
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<UptimeService>();
+        services.AddMvc().AddMvcOptions(options => {
+            options.RespectBrowserAcceptHeader = true;
+        });
+    }
+
+    public void ConfigureDevelopmentServices(IServiceCollection services)
+    {
+        services.AddSingleton<UptimeService>();
+        services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if ((Configuration.GetSection("ShortCircuitMiddleware")?
+            .GetValue<bool>("EnableBrowserShortCircuit")).Value)
         {
+            app.UseMiddleware<BrowserTypeMiddleware>();
+            app.UseMiddleware<ShortCircuitMiddleware>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        if (env.IsDevelopment())
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseBrowserLink();
         }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        // добавляем поддержку каталога node_modules
+        app.UseFileServer(new FileServerOptions()
+        {
+            FileProvider = new PhysicalFileProvider(
+                Path.Combine(env.ContentRootPath, "node_modules")
+            ),
+            RequestPath = "/node_modules",
+            EnableDirectoryBrowsing = false
+        });
+
+        app.UseMvc(routes =>
+        {
+            routes.MapRoute(
+            name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+
+    public void ConfigureDevelopment(IApplicationBuilder app,
+    IHostingEnvironment env)
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseStatusCodePages();
+        app.UseBrowserLink();
+        app.UseStaticFiles();
+        app.UseMvcWithDefaultRoute();
     }
 }
